471140741f9365329dd31e38fb76b4dd655e2d5d
qweri0p <71005650+qweri0p@users.noreply.github.com>
2024-06-14 17:39:52 +0200

    wow

--- get.py
+++ get.py
@@ -2,21 +2,11 @@ import requests, json
 
 headers={"Content-Type": "application/json"}
 
-sessionSetup = requests.post("http://localhost:8191/v1", headers=headers, json={"cmd": "sessions.create"})
-sessionRawData = sessionSetup.json()
-print(sessionRawData["message"])
-
-sessionId = sessionRawData["session"]
-
-result = requests.post("http://localhost:8191/v1", headers=headers, json={"cmd": "request.get", "url": "https://nhentai.net/api/galleries/search?query=pages:>0", "session": sessionId})
+result = requests.get("https://nhentai.net/api/galleries/search?query=pages:>0", headers=headers)
 rawResult = result.json()
 
-
-
-resultString = rawResult["solution"]["response"]
-json_string = resultString[131:-20]
-json_data = json.loads(json_string)
-amountofpages = json_data["num_pages"]
+amountofpages = rawResult["num_pages"]
+print(f"There are {amountofpages} pages of manga on nhentai A.T.M.")
 
 fulllink = "https://nhentai.net/api/galleries/search?query=pages:%3E0?page="
 
@@ -26,19 +16,14 @@ running = True
 
 index = 1
 while index < int(amountofpages):
-    result = requests.post("http://localhost:8191/v1", headers=headers, json={"cmd": "request.get", "url": "https://nhentai.net/api/galleries/search?query=pages:>0&page="+str(index), "session": sessionId})
+    result = requests.get(f"https://nhentai.net/api/galleries/search?query=pages:>0&page={str(index)}", headers=headers)
     if result.status_code == 200:
         rawResult = result.json()
-
-        resultString = rawResult["solution"]["response"]
-        json_string = resultString[131:-20]
+        js = json.dumps(rawResult, sort_keys=True, indent=4, separators=(',', ': '))
         print("Downloading: "+result.url)
-        with open(dirname+"/"+str(index)+".json", 'wb') as f:
-            f.write(bytes(json_string.encode('utf8')))
+        with open(dirname+"/"+str(index)+".json", 'w', encoding="utf-8") as f:
+            json.dump(rawResult, f, ensure_ascii=False, indent=4)
     else:
         running = False
     index+=1
 
-sessionDestroy = requests.post("http://localhost:8191/v1", headers=headers, json={"cmd": "sessions.destroy", "session": sessionId})
-sessionDestroyRaw = sessionDestroy.json()
-print(sessionDestroyRaw["message"])
